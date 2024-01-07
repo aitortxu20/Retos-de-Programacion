73f6bf861ce1355657988faa04f010dcdb1f1418
Aitor <69603173+aitortxu20@users.noreply.github.com>
2024-01-07 22:57:54 +0100

    Excercise #32 translated

--- #32/#32.py
+++ #32/#32.py
@@ -1,3 +1,10 @@
+# /*
+#  * Create a function that calculates the column number in an Excel sheet
+#  * based on its name.
+#  * - Columns are designated by letters from "A" to "Z" infinitely.
+#  * - Examples: A = 1, Z = 26, AA = 27, CA = 79.
+#  */
+
 def excel_column_to_number(column):
     letters = {chr(i): i - 64 for i in range(65, 91)}
     result = 0
